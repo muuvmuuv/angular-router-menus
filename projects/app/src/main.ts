cdbd9d36e67ee9dd8a8da9572bb3b9a0e4b88843
Marvin Heilemann <11534760+muuvmuuv@users.noreply.github.com>
2025-04-28 21:06:15 +0200

    Add some good defaults

--- projects/app/src/main.ts
+++ projects/app/src/main.ts
@@ -2,21 +2,27 @@ import { provideHttpClient, withFetch } from "@angular/common/http"
 import { provideExperimentalZonelessChangeDetection } from "@angular/core"
 import { bootstrapApplication } from "@angular/platform-browser"
 import { provideAnimationsAsync } from "@angular/platform-browser/animations/async"
-import { provideRouter, withComponentInputBinding } from "@angular/router"
+import {
+	NoPreloading,
+	provideRouter,
+	withComponentInputBinding,
+	withPreloading,
+} from "@angular/router"
 
 import { provideRouterMenus } from "angular-router-menus"
 
 import { AppComponent } from "./app/app.component"
+import { environment } from "./environments/environment"
 import { menus } from "./menus"
 import { routes } from "./routes"
 
 void bootstrapApplication(AppComponent, {
 	providers: [
 		provideExperimentalZonelessChangeDetection(),
-		provideRouter(routes, withComponentInputBinding()),
+		provideRouter(routes, withPreloading(NoPreloading), withComponentInputBinding()),
 		provideRouterMenus(routes, menus, {
 			defaultMenu: "main",
-			debug: false,
+			debug: !environment.production,
 		}),
 		provideAnimationsAsync(),
 		provideHttpClient(withFetch()),
